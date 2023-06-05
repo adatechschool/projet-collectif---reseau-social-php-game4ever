c6dea933a1e4f0b0e99823aed8f68ea07b0b0f35
accardigianni <gianni.accardi@epitech.eu>
2023-06-05 12:39:58 +0200

    feat : input modfie en button

--- wall.php
+++ wall.php
@@ -5,165 +5,161 @@ $title = "Mur";
 
 <!doctype html>
 <html lang="fr">
-    <head>
-        <meta charset="utf-8">
-        <title>ReSoC - Mur</title> 
-        <meta name="author" content="Julien Falconnet">
-        <link rel="stylesheet" href="style.css"/>
-    </head>
-    <body>
-        <?php include 'includes/header.php' ?>
 
-        <div id="wrapper">
-            
+<head>
+    <meta charset="utf-8">
+    <title>ReSoC - Mur</title>
+    <meta name="author" content="Julien Falconnet">
+    <link rel="stylesheet" href="style.css" />
+</head>
 
-            <?php include 'includes/aside_user.php' ?>
-            <main>
-                <?php
-                if ($user_id == $_SESSION['connected_id']) {
-                    // $_POST['new_post] is retrieved when we click on the button in the form further bellow
-                    $enCoursDeTraitement = isset($_POST['new_post']);
-                    if ($enCoursDeTraitement && $_POST['new_post'] !== "") {
-                        $author_id = $user_id;
-                        $post_content = $_POST['new_post'];
-                        // Check for SQL injection
-                        $post_content = $mysqli->real_escape_string(($post_content));
+<body>
+    <?php include 'includes/header.php' ?>
 
-                        // Query to insert the post in the DB (DataBase)
-                        $lInstructionSql = "INSERT INTO posts "
-                            . "(user_id, content, created, parent_id) "
-                            . "VALUES ("
-                            . $author_id . ","
-                            . "'" . $post_content . "', "
-                            . "NOW(), "
-                            . "NULL);";
-
-                        $ok = $mysqli->query($lInstructionSql);
-                        if ( ! $ok)
-                        {
-                            echo "Impossible d'ajouter le message: " . $mysqli->error;
-                        } else
-                        {
-                            // Get the id of the post we just created in the DB
-                            $post_id = $mysqli->insert_id;
-                            echo "Message posté en tant que : " . $user["alias"];
-                        }
-
-                        // We want to extract the tags # from the message we just posted
-                        // Tags extraction from user post
-                        preg_match_all("/#(\w+)/", $post_content, $post_tag_list);
-                        $post_tag_list = $post_tag_list[1];
-                        // Join the list of tag for the query
-                        $tags_joined = "'" . implode("','", $post_tag_list) . "'";
-
-                        // SQL request to get a list of already existing tags and another list of their ID
-                        $sqlGetTagList = "SELECT id, label FROM tags WHERE label IN (" . $tags_joined . ");";
-                        $tags_result = $mysqli->query($sqlGetTagList);
-                        $existing_tags = array();
-                        $existing_tags_id = array();
-                        while($row = $tags_result->fetch_assoc()) {
-                            $existing_tags[] = $row['label'];
-                            $existing_tags_id[] = $row['id'];
-                        }
-
-                        // Get the list of tags that user used but isn't in the DB
-                        $tags_to_insert = array_diff($post_tag_list, $existing_tags);
+    <div id="wrapper">
 
 
-                        function link_post_to_tag($tag_id, $post_id) {
-                            $sqlLinkPostTag = "INSERT INTO posts_tags "
-                                . "(post_id, tag_id) "
-                                . "VALUES ('" . $post_id . "', '"
-                                . $tag_id . "');";
+        <?php include 'includes/aside_user.php' ?>
+        <main>
+            <?php
+            if ($user_id == $_SESSION['connected_id']) {
+                // $_POST['new_post] is retrieved when we click on the button in the form further bellow
+                $enCoursDeTraitement = isset($_POST['new_post']);
+                if ($enCoursDeTraitement && $_POST['new_post'] !== "") {
+                    $author_id = $user_id;
+                    $post_content = $_POST['new_post'];
+                    // Check for SQL injection
+                    $post_content = $mysqli->real_escape_string(($post_content));
 
-                            $mysqli = new mysqli("localhost", "root", "root", "socialnetwork");
-                            $sqlResult = $mysqli->query($sqlLinkPostTag);
-                            if (! $sqlResult) {
-                                echo $mysqli->error;
-                            }
-                        }
+                    // Query to insert the post in the DB (DataBase)
+                    $lInstructionSql = "INSERT INTO posts "
+                        . "(user_id, content, created, parent_id) "
+                        . "VALUES ("
+                        . $author_id . ","
+                        . "'" . $post_content . "', "
+                        . "NOW(), "
+                        . "NULL);";
 
-                        // Link tag_id and post_id in the posts_tags table
-                        // For existing tags
-                        if ($existing_tags !== [""] && $existing_tags !== []) {
-                            foreach ($existing_tags_id as $tag_id) {
-                                link_post_to_tag($tag_id, $post_id);
-                            }
-                        }
-
-                        // Add new tag
-                        if ($tags_to_insert !== [""] && $tags_to_insert !== []) {
-                            foreach($tags_to_insert as $new_tag) {
-                                $sqlAddNewTag = "INSERT INTO tags (label) VALUES ('" . $new_tag . "');";
-                                $ok = $mysqli->query($sqlAddNewTag);
-                                if ( ! $ok)
-                                {
-                                    echo "Impossible d'ajouter le tag : " . $mysqli->error;
-                                } else {
-                                    // Get the id of the tag we just created
-                                    $tag_id = $mysqli->insert_id;
-
-                                    // Link the new tag to the post
-                                    link_post_to_tag($tag_id, $post_id);
-                                }
-                            }
-                        }
-
+                    $ok = $mysqli->query($lInstructionSql);
+                    if (!$ok) {
+                        echo "Impossible d'ajouter le message: " . $mysqli->error;
+                    } else {
+                        // Get the id of the post we just created in the DB
+                        $post_id = $mysqli->insert_id;
+                        echo "Message posté en tant que : " . $user["alias"];
                     }
-                    ?>
-                    <article>
-                        <form action="wall.php" method="post">
-                            <dl>
-                                <dt><label for="new_post">Avez-vous quelque chose à dire ?</label></dt>
-                                <dd><textarea name="new_post"></textarea></dd>
-                            </dl>
-                            <input type='submit' id="submit_form">
-                        </form>
-                    </article>
-                <?php
-                } else {
-                    if ($_SERVER['REQUEST_METHOD'] === "POST") 
-                    { 
-                        // If the subscribe button is pressed
-                        if (isset($_POST['subscribe_button'])) {
-                            $sqlInstruction = "INSERT INTO followers "
-                                . "(followed_user_id, following_user_id) "
-                                . "VALUES ("
-                                . $user_id . ", "
-                                . $_SESSION['connected_id'] . ");";
 
-                            $ok = $mysqli->query($sqlInstruction);
-                            if ( ! $ok)
-                            {
-                                echo "<article>Impossible de s'abonner : " . $mysqli->error . "</article>";
+                    // We want to extract the tags # from the message we just posted
+                    // Tags extraction from user post
+                    preg_match_all("/#(\w+)/", $post_content, $post_tag_list);
+                    $post_tag_list = $post_tag_list[1];
+                    // Join the list of tag for the query
+                    $tags_joined = "'" . implode("','", $post_tag_list) . "'";
+
+                    // SQL request to get a list of already existing tags and another list of their ID
+                    $sqlGetTagList = "SELECT id, label FROM tags WHERE label IN (" . $tags_joined . ");";
+                    $tags_result = $mysqli->query($sqlGetTagList);
+                    $existing_tags = array();
+                    $existing_tags_id = array();
+                    while ($row = $tags_result->fetch_assoc()) {
+                        $existing_tags[] = $row['label'];
+                        $existing_tags_id[] = $row['id'];
+                    }
+
+                    // Get the list of tags that user used but isn't in the DB
+                    $tags_to_insert = array_diff($post_tag_list, $existing_tags);
+
+
+                    function link_post_to_tag($tag_id, $post_id)
+                    {
+                        $sqlLinkPostTag = "INSERT INTO posts_tags "
+                            . "(post_id, tag_id) "
+                            . "VALUES ('" . $post_id . "', '"
+                            . $tag_id . "');";
+
+                        $mysqli = new mysqli("localhost", "root", "root", "socialnetwork");
+                        $sqlResult = $mysqli->query($sqlLinkPostTag);
+                        if (!$sqlResult) {
+                            echo $mysqli->error;
+                        }
+                    }
+
+                    // Link tag_id and post_id in the posts_tags table
+                    // For existing tags
+                    if ($existing_tags !== [""] && $existing_tags !== []) {
+                        foreach ($existing_tags_id as $tag_id) {
+                            link_post_to_tag($tag_id, $post_id);
+                        }
+                    }
+
+                    // Add new tag
+                    if ($tags_to_insert !== [""] && $tags_to_insert !== []) {
+                        foreach ($tags_to_insert as $new_tag) {
+                            $sqlAddNewTag = "INSERT INTO tags (label) VALUES ('" . $new_tag . "');";
+                            $ok = $mysqli->query($sqlAddNewTag);
+                            if (!$ok) {
+                                echo "Impossible d'ajouter le tag : " . $mysqli->error;
                             } else {
-                                // Reload the current page
-                                header("Location: ".$_SERVER['PHP_SELF']."?".$_SERVER['QUERY_STRING']);
-                                exit();
-                            }
-                        } 
-                        // If the unsubscribe button is pressed
-                        else if (isset($_POST['unsubscribe_button'])) {
-                            $sqlInstruction = "DELETE FROM followers "
-                                . "WHERE followed_user_id = " . $user_id
-                                . " AND following_user_id = " . $_SESSION['connected_id'] . ";";
-                            $ok = $mysqli->query($sqlInstruction);
-                            if ( ! $ok)
-                            {
-                                echo "<article>Impossible de se désabonner : " . $mysqli->error . "</article>";
-                            } else {
-                                header("Location: ".$_SERVER['PHP_SELF']."?".$_SERVER['QUERY_STRING']);
-                                exit();
+                                // Get the id of the tag we just created
+                                $tag_id = $mysqli->insert_id;
+
+                                // Link the new tag to the post
+                                link_post_to_tag($tag_id, $post_id);
                             }
                         }
                     }
                 }
+            ?>
+                <article>
+                    <form action="wall.php" method="post">
+                        <dl>
+                            <dt><label for="new_post">Avez-vous quelque chose à dire ?</label></dt>
+                            <dd><textarea name="new_post"></textarea></dd>
+                        </dl>
+                        <button type="submit" id="submit_form">Poster</button>
+                    </form>
+                </article>
+            <?php
+            } else {
+                if ($_SERVER['REQUEST_METHOD'] === "POST") {
+                    // If the subscribe button is pressed
+                    if (isset($_POST['subscribe_button'])) {
+                        $sqlInstruction = "INSERT INTO followers "
+                            . "(followed_user_id, following_user_id) "
+                            . "VALUES ("
+                            . $user_id . ", "
+                            . $_SESSION['connected_id'] . ");";
 
-                // Like/Unlike function
-                include "includes/like_unlike.php";
-                
-                // Get posts from the DB and show them on the wall page
-                $laQuestionEnSql = "
+                        $ok = $mysqli->query($sqlInstruction);
+                        if (!$ok) {
+                            echo "<article>Impossible de s'abonner : " . $mysqli->error . "</article>";
+                        } else {
+                            // Reload the current page
+                            header("Location: " . $_SERVER['PHP_SELF'] . "?" . $_SERVER['QUERY_STRING']);
+                            exit();
+                        }
+                    }
+                    // If the unsubscribe button is pressed
+                    else if (isset($_POST['unsubscribe_button'])) {
+                        $sqlInstruction = "DELETE FROM followers "
+                            . "WHERE followed_user_id = " . $user_id
+                            . " AND following_user_id = " . $_SESSION['connected_id'] . ";";
+                        $ok = $mysqli->query($sqlInstruction);
+                        if (!$ok) {
+                            echo "<article>Impossible de se désabonner : " . $mysqli->error . "</article>";
+                        } else {
+                            header("Location: " . $_SERVER['PHP_SELF'] . "?" . $_SERVER['QUERY_STRING']);
+                            exit();
+                        }
+                    }
+                }
+            }
+
+            // Like/Unlike function
+            include "includes/like_unlike.php";
+
+            // Get posts from the DB and show them on the wall page
+            $laQuestionEnSql = "
                     SELECT posts.id AS post_id, posts.content, posts.created, users.alias as author_name, users.id AS post_user_id,
                     COUNT(likes.id) as like_number, GROUP_CONCAT(DISTINCT tags.label) AS taglist 
                     FROM posts
@@ -176,16 +172,16 @@ $title = "Mur";
                     ORDER BY posts.created DESC 
                     LIMIT 20;  
                     ";
-                $lesInformations = $mysqli->query($laQuestionEnSql);
-                if ( ! $lesInformations)
-                {
-                    echo("Échec de la requete : " . $mysqli->error);
-                }
+            $lesInformations = $mysqli->query($laQuestionEnSql);
+            if (!$lesInformations) {
+                echo ("Échec de la requete : " . $mysqli->error);
+            }
 
-                // Show the posts
-                include 'includes/article.php' ?>
+            // Show the posts
+            include 'includes/article.php' ?>
 
-            </main>
-        </div>
-    </body>
+        </main>
+    </div>
+</body>
+
 </html>
