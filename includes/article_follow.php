1ca64e8bd9aa79499c028d4dfed3de71620822c9
accardigianni <gianni.accardi@epitech.eu>
2023-06-06 23:07:30 +0200

    design css a valide + menu deroulant du profil a refaire

--- includes/article_follow.php
+++ includes/article_follow.php
@@ -1,9 +1,9 @@
 <?php while ($follow = $lesInformations->fetch_assoc()) {
 ?>
 
-<article>
-    <img src="user.jpg" alt="blason"/>
-    <h3><?php echo $follow['alias'] ?></h3>
-    <p><?php echo $follow['id'] ?></p>
-</article>
+    <article>
+        <img src="image/profilpicture.jpg" alt="blason" />
+        <h3><?php echo $follow['alias'] ?></h3>
+        <p><?php echo $follow['id'] ?></p>
+    </article>
 <?php } ?>
--- subscriptions.php
+++ subscriptions.php
@@ -5,43 +5,38 @@ $title = "Mes abonnements"
 
 <!doctype html>
 <html lang="fr">
-    <head>
-        <meta charset="utf-8">
-        <title>ReSoC - Mes abonnements</title> 
-        <meta name="author" content="Julien Falconnet">
-        <link rel="stylesheet" href="style.css"/>
-    </head>
-    <body>
-        <?php include 'includes/header.php' ?>
-        <div id="wrapper">
-            <aside>
-                <img src="user.jpg" alt="Portrait de l'utilisatrice"/>
-                <section>
-                    <h3>Présentation</h3>
-                    <p>Sur cette page vous trouverez la liste des personnes dont
-                        l'utilisatrice
-                        n° <?php echo $_SESSION['connected_id'] ?>
-                        suit les messages
-                    </p>
 
-                </section>
-            </aside>
-            <main class='contacts'>
-                <?php
-                // Get the user we follow
-                $laQuestionEnSql = "
+<head>
+    <meta charset="utf-8">
+    <title>ReSoC - Mes abonnements</title>
+    <meta name="author" content="Julien Falconnet">
+    <link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+    <?php include 'includes/header.php' ?>
+    <div id="wrapper">
+        <?php include 'includes/aside_user.php' ?>
+
+        </section>
+        </aside>
+        <main class='contacts'>
+            <?php
+            // Get the user we follow
+            $laQuestionEnSql = "
                     SELECT users.* 
                     FROM followers 
                     LEFT JOIN users ON users.id=followers.followed_user_id 
                     WHERE followers.following_user_id='$user_id'
                     GROUP BY users.id
                     ";
-                $lesInformations = $mysqli->query($laQuestionEnSql);
+            $lesInformations = $mysqli->query($laQuestionEnSql);
 
-                // Show the posts
-                include 'includes/article_follow.php';
-                ?>
-            </main>
-        </div>
-    </body>
+            // Show the posts
+            include 'includes/article_follow.php';
+            ?>
+        </main>
+    </div>
+</body>
+
 </html>
